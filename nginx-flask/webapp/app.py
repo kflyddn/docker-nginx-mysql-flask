13fd9bce8d7c58b9eebcbd28f0248f19b21d4339
kflyddn <kflyddn@gmail.com>
2018-05-29 01:21:45 +0800

    merge flask dockerized to my project

--- nginx-flask/webapp/app.py
+++ nginx-flask/webapp/app.py
@@ -1,22 +1,43 @@
-from flask import Flask, request, render_template
+#!/usr/bin/env python
 
-app = Flask(__name__)
+"""Minimal Flask app."""
+
+from flask import Flask
+from flask import render_template, request
+import logging
+import sys
+import db
+from sqlalchemy import Table
+from os import listdir
+from os.path import isfile, join
+
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
+                    level=logging.INFO,
+                    stream=sys.stdout)
+
+app = Flask(__name__, template_folder='template', static_url_path='')
+
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
-    return render_template('home.html')
+def index_controller():
+    """Show a form to enter information what to look for."""
+    if request.form.get('country_name'):
+        countries_table = Table('countries', db.meta)
+        country_name = request.form.get('country_name')
+        ins = countries_table.insert().values(country_name=country_name)
+        db.conn.execute(ins)
+    mypath = '/uploads'
+    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    return render_template('home.html', active_page="home", files=onlyfiles)
 
-@app.route('/signin', methods=['GET'])
-def signin_form():
-    return render_template('form.html')
 
-@app.route('/signin', methods=['POST'])
-def signin():
-    username = request.form['username']
-    password = request.form['password']
-    if username=='admin' and password=='zbzb':
-        return render_template('signin-ok.html', username='abcd')
-    return render_template('form.html', message='Bad username or password', username=username)
+@app.route('/list')
+def list_controller():
+    """Show a list of all countries."""
+    result = db.engine.execute("SELECT * FROM `countries`")
+    data = [country[0] for country in result]
+
+    return render_template('country_list.html', active_page="list", data=data)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host='0.0.0.0', threaded=True)
